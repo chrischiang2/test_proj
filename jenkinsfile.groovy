37bbc537ac6d547dccc64289e055b3cf4413c207
chrischiang2 <86010269+chrischiang2@users.noreply.github.com>
2021-08-26 00:21:07 +0800

    Add files via upload
    add in jenkins file

---
+++ jenkinsfile.groovy
@@ -0,0 +1,56 @@
+// Import the needed libraries and pull in additional requried libraries
+// https://confluence.it.keysight.com/display/trustForgeCorePipelineLibrary
+@Library('TrustForge Pipeline Library') _
+
+pipeline {
+    agent any
+    stages {
+        stage('Build') {
+            steps {
+                sh 'echo "Hello World"'
+                sh '''
+                    echo "Multiline shell steps works too"
+                    ls -lah
+                '''
+            }
+        }
+        
+        stage('Checkout cyclop yaml') {
+            steps {
+                dir('cyclop_yaml') {
+                    git branch: 'cyclop-linux',
+                        url: 'ssh://git@bitbucket.it.keysight.com:7999/cyclop/cyclop_yaml.git'
+                        
+                    sh "ls -lat"    
+                }
+            }
+        }
+        
+        stage('Checkout bitbake script') {
+            steps {
+                dir('bitbake_script') {
+                    git branch: 'develop',
+                        url: 'ssh://git@bitbucket.it.keysight.com:7999/myve/bitbake-scripts.git'
+                        
+                    sh "ls -lat"    
+                }
+            }
+        }
+        
+        stage('Prepare bitbake') {
+            steps {
+                dir('bitbake_script') {
+                    sh "./buildtask_prepare_bitbake_workspace.py --bitbake_manifest ../cyclop_yaml/p700-cyclop.yml --bitbake_workspace ~/hpp-dev"    
+                }
+            }     
+        }
+        
+        stage('Starting bitbake') {
+            steps {
+                dir('bitbake_script') {
+                    sh "./buildtask_bitbake.py --bitbake_manifest ../cyclop_yaml/p700-cyclop.yml --bitbake_workspace ~/hpp-dev --bitbake_target instrument"    
+                }
+            }     
+        }        
+    }
+}
